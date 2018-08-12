0485ba751445bc062b037a5d2e09da9c5a9fcf9a
hgirish <girish@post.com>
2018-08-12 10:55:10 -0700

    Add project files.

---
+++ MagickNet.Sample/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MagickNet.Sample.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
