43f58fce561ae8cbf9a403f630a7b71ae6548249
hgirish <girish@post.com>
2018-08-12 22:03:17 -0700

    MagickNet.Sample initial project

--- MagickNet.Sample/Controllers/HomeController.cs
+++ MagickNet.Sample/Controllers/HomeController.cs
@@ -5,11 +5,20 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using MagickNet.Sample.Models;
+using ImageMagick;
+using Microsoft.AspNetCore.Hosting;
+using System.IO;
 
 namespace MagickNet.Sample.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly IHostingEnvironment _hostingEnvironment;
+
+        public HomeController(IHostingEnvironment hostingEnvironment)
+        {
+            _hostingEnvironment = hostingEnvironment;
+        }
         public IActionResult Index()
         {
             return View();
@@ -17,7 +26,39 @@ namespace MagickNet.Sample.Controllers
 
         public IActionResult About()
         {
+            var rootpath = _hostingEnvironment.ContentRootPath;
+            var webRootPath = _hostingEnvironment.WebRootPath;
             ViewData["Message"] = "Your application description page.";
+            string filePath = Path.Combine(webRootPath, "images", "imagesharp-logo.png");
+            string newFilePath = Path.Combine(webRootPath, "images", "watermark-image.png");
+            using (MagickImage image = new MagickImage(filePath))
+            {
+                // image.Draw(drawables);
+                var drawable = new Drawables()
+                    .FontPointSize(72)
+                    .Font(@"D:\Downloads\Pacifico-Regular.ttf")
+                    .StrokeColor(MagickColors.Orange)
+                    .TextAlignment(TextAlignment.Center)
+                    .Text(256, 64, "Magick.NET")
+                    .StrokeColor(new MagickColor(0, Quantum.Max, 0))
+                    .FillColor(MagickColors.SaddleBrown)
+                    .Ellipse(256, 96, 192, 8, 0, 360)
+                    .Draw(image);
+                image.Write(newFilePath);
+                ExifProfile profile = image.GetExifProfile();
+                if (profile == null)
+                {
+                    ViewData["Message"] = "Image does not contain exif information";
+                }
+                else
+                {
+                    string message = "";
+                    foreach (ExifValue value in profile.Values)
+                    {
+                        message += $"{value.Tag} ({value.DataType}): {value.ToString()}<br />";
+                    }
+                }
+            }
 
             return View();
         }
